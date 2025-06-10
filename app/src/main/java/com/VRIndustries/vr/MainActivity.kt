80edc89a02b54cd6d000317481511b83cebe4371
Yaswanth Ravi Teja Dammalapati <yaswanth@Yaswanths-MacBook-Air.local>
2025-06-10 02:36:34 -0400

    Basic navigation setup is done

--- app/src/main/java/com/VRIndustries/vr/MainActivity.kt
+++ app/src/main/java/com/VRIndustries/vr/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.VRIndustries.vr
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.VRIndustries.vr.databinding.ActivityMainBinding
+import com.VRIndustries.vr.fragments.CartFragment
+import com.VRIndustries.vr.fragments.HomeFragment
+import com.VRIndustries.vr.fragments.ProductsFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loadFragment(HomeFragment())
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> loadFragment(HomeFragment())
+                R.id.nav_products -> loadFragment(ProductsFragment())
+                R.id.nav_cart -> loadFragment(CartFragment())
+            }
+            true
         }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
